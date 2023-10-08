0808dd4a5f861acad2fb83629a13297daa1c114b
ShibanshL <ShibanshL@users.noreply.github.com>
2023-10-08 18:23:36 +0000

    deploy: f05d6389f998b80a3620f909d41f422a349d9d61

--- assets/AppPageURLError-86709ec8.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-519f31f6.js";function i(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"Loading",children:e.jsx("h1",{children:"Please check the link before using it."})})})}export{i as default};
