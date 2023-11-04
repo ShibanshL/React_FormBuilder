0d2acb1a34867b1fbc330433d4b56549fc553343
ShibanshL <ShibanshL@users.noreply.github.com>
2023-11-04 07:56:15 +0000

    deploy: b6eb702570db33325b8cdc133d8aa94e6954d498

---
+++ assets/AppPageURLError-c0ea78c2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-89c4c720.js";function i(){return e.jsx(e.Fragment,{children:e.jsx("div",{className:"Loading",children:e.jsx("h1",{children:"Please check the link before using it."})})})}export{i as default};
--- assets/Cross 2-5fe52e58.js
+++
@@ -1,1 +0,0 @@
-const s="/React_FormBuilder/assets/Cross 2-3f166b3e.svg";export{s as C};
